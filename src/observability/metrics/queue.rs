70fd205f40a82c062e20ebfcd500d2cb17de555b
Karan Balani <29383381+balanikaran@users.noreply.github.com>
2026-07-27 23:45:10 +0530

    refactor: simplify queue instrumentation sharing

--- src/observability/metrics/queue.rs
+++ src/observability/metrics/queue.rs
@@ -8,45 +8,55 @@ use super::{
 
 #[derive(Clone)]
 pub(crate) struct QueueInstrumentation {
+    inner: Arc<QueueInstrumentationInner>,
+}
+
+struct QueueInstrumentationInner {
     meter: Meter,
-    commands: Arc<OnceLock<QueueCommandMetrics>>,
-    visibility_timeout: Arc<OnceLock<VisibilityTimeoutMetrics>>,
-    expired_message_cleaner: Arc<OnceLock<ExpiredMessageCleanerMetrics>>,
-    state: Arc<OnceLock<QueueStateMetrics>>,
+    commands: OnceLock<QueueCommandMetrics>,
+    visibility_timeout: OnceLock<VisibilityTimeoutMetrics>,
+    expired_message_cleaner: OnceLock<ExpiredMessageCleanerMetrics>,
+    state: OnceLock<QueueStateMetrics>,
 }
 
 impl QueueInstrumentation {
     pub(super) fn new(meter: &Meter) -> Self {
         Self {
-            meter: meter.clone(),
-            commands: Arc::new(OnceLock::new()),
-            visibility_timeout: Arc::new(OnceLock::new()),
-            expired_message_cleaner: Arc::new(OnceLock::new()),
-            state: Arc::new(OnceLock::new()),
+            inner: Arc::new(QueueInstrumentationInner {
+                meter: meter.clone(),
+                commands: OnceLock::new(),
+                visibility_timeout: OnceLock::new(),
+                expired_message_cleaner: OnceLock::new(),
+                state: OnceLock::new(),
+            }),
         }
     }
 
     pub(crate) fn commands(&self) -> QueueCommandMetrics {
-        self.commands
-            .get_or_init(|| QueueCommandMetrics::new(&self.meter))
+        self.inner
+            .commands
+            .get_or_init(|| QueueCommandMetrics::new(&self.inner.meter))
             .clone()
     }
 
     pub(crate) fn visibility_timeout(&self) -> VisibilityTimeoutMetrics {
-        self.visibility_timeout
-            .get_or_init(|| VisibilityTimeoutMetrics::new(&self.meter))
+        self.inner
+            .visibility_timeout
+            .get_or_init(|| VisibilityTimeoutMetrics::new(&self.inner.meter))
             .clone()
     }
 
     pub(crate) fn expired_message_cleaner(&self) -> ExpiredMessageCleanerMetrics {
-        self.expired_message_cleaner
-            .get_or_init(|| ExpiredMessageCleanerMetrics::new(&self.meter))
+        self.inner
+            .expired_message_cleaner
+            .get_or_init(|| ExpiredMessageCleanerMetrics::new(&self.inner.meter))
             .clone()
     }
 
     pub(crate) fn state(&self) -> QueueStateMetrics {
-        self.state
-            .get_or_init(|| QueueStateMetrics::new(&self.meter))
+        self.inner
+            .state
+            .get_or_init(|| QueueStateMetrics::new(&self.inner.meter))
             .clone()
     }
 }
